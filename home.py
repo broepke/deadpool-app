c4a44f97ff58813efb6b2e1bff473a5832bc99de
Brian Roepke <broepke@gmail.com>
2023-12-12 12:27:07 -0800

    Session state "fix"

--- home.py
+++ home.py
@@ -4,33 +4,54 @@ Streamlit main app
 import hmac
 import streamlit as st
 
+def save_value(key):
+    st.session_state[key] = st.session_state["_" + key]
+
+
+def get_value(key):
+    st.session_state["_" + key] = st.session_state[key]
+
 
 def check_password():
-    """Returns `True` if the user had the correct password."""
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="_username")
+            st.text_input("Password", type="password", key="_password")
+            st.form_submit_button("Log in", on_click=password_entered)
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+        if st.session_state["_username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["_password"],
+            st.secrets.passwords[st.session_state["_username"]],
+        ):
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the password.
+            save_value("username")
+            del st.session_state["_password"]  # Don't store the username or password.
+            del st.session_state["_username"]
         else:
             st.session_state["password_correct"] = False
 
-    # Return True if the passward is validated.
+    # Return True if the username + password is validated.
     if st.session_state.get("password_correct", False):
         return True
 
-    # Show input for password.
-    st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
-    )
+    # Show inputs for username + password.
+    login_form()
     if "password_correct" in st.session_state:
-        st.error("😕 Password incorrect")
+        st.error("😕 User not known or password incorrect")
     return False
 
 
 if not check_password():
-    st.stop()  # Do not continue if check_password is not True.
+    st.stop()
+
+
 
 # Initialize connection.
 conn = st.connection("snowflake")
@@ -47,9 +68,9 @@ df_players = load_players_table()
 # Add some text
 st.title("Deadpool 2024 :skull_and_crossbones:")
 
+st.write("Current User: " + st.session_state["username"])
+
 st.image("deadpool.png", "Deadpool 2024")
 
 st.subheader("Current Players:")
 st.dataframe(df_players)
-
-
--- pages/drafting.py
+++ pages/drafting.py
@@ -7,18 +7,22 @@ if not check_password():
 
 conn = st.connection("snowflake")
 
-df = conn.query(
-    """select
-        concat(first_name || ' ' || left(last_name, 1) || '.') as PLAYER, EMAIL 
-    from PLAYERS
-    order by 1
-    """,
-    ttl=600,
-)
+# df = conn.query(
+#     """select
+#         concat(first_name || ' ' || left(last_name, 1) || '.') as PLAYER, EMAIL
+#     from PLAYERS
+#     order by 1
+#     """,
+#     ttl=600,
+# )
 
-players = df["PLAYER"].tolist()
+# players = df["PLAYER"].tolist()
 
-submitter = st.radio("Please choose your name", players, index=None)
+try:
+    st.write(st.session_state["username"])
+    email = st.session_state["username"]
+except:
+    st.write("Please login again")
 
 
 st.subheader("Draft Picks:")
@@ -29,17 +33,10 @@ with st.form("Draft Picks"):
     if submitted:
         st.write("Draft Pick:", pick)
 
-        # Filter the DataFrame
-        filtered_df = df[df['PLAYER'] == submitter]
+        write_query = (
+            f"INSERT INTO picks (name, picked_by) VALUES ('{pick}', '{email}')"
+        )
 
-        # Check if the filtered DataFrame is empty
-        if not filtered_df.empty:
-            email = filtered_df['EMAIL'].iloc[0]
-        else:
-            print(f"No email found for {submitter}")
-        
-        write_query = f"INSERT INTO picks (name, picked_by) VALUES ('{pick}', '{email}')"
-        
         st.write(write_query)
 
         conn.cursor().execute(write_query)
