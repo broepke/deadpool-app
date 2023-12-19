7c15c176c941f9975cfa347ad31e5f470ed42ded
Brian Roepke <broepke@gmail.com>
2023-12-19 09:30:05 -0600

    Rule updates

--- pages/5_Drafting.py
+++ pages/5_Drafting.py
@@ -108,10 +108,8 @@ if draft_logic(email):
 
 st.divider()
 
-st.subheader("Tip for better pick entry")
-
-st.markdown(
-    """For the best results, use the same spelling of the person from their Wikipedia page. For example, Chris Burke was chosen in the past, his Wiki page's title is **"Chris Burke (actor)"** - if you enter this, it will speed up the disambiguation of the pick. Not critical but the arbiter will appreciate it."""
-)
-
-st.image("wiki.png", "Wikipedia Page Naming Convention")
+st.markdown("""
+**Notes**: 
+- The system checks for duplicate entries and has built-in fuzzy matching of names entered.  If it's a slight misspelling, the duplicate will be caught.  If it's way off, the Arbiter must duplicate and resolve it after draft day.  The person with the earlier timestamp on the pick will keep the pick.  The other person will get to submit an additional pick.
+- Please do not pick a dead person.  If you do, you will lose that pick and receive 0 points.
+""")
