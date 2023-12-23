c85dbc6bf63f479b59cc9a5fd0724fc4c2a5a459
Brian Roepke <broepke@gmail.com>
2023-12-23 07:40:04 -0600

    table names

--- pages/6_Leaderboard.py
+++ pages/6_Leaderboard.py
@@ -21,8 +21,8 @@ except KeyError:
 conn = st.connection("snowflake")
 
 
-df_score_2024 = load_snowflake_table(conn, "score_twenty_four")
-df_score_2023 = load_snowflake_table(conn, "score_twenty_three")
+df_score_2024 = load_snowflake_table(conn, "SCORE_TWENTY_FOUR")
+df_score_2023 = load_snowflake_table(conn, "SCORE_TWENTY_THREE")
 
 
 st.title("2024 Leaderboard:")
