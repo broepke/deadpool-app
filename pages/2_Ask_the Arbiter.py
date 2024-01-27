fc9a564060437a5e30254803e1079be7add9097f
Brian Roepke <broepke@gmail.com>
2024-01-27 17:30:49 -0600

    Migration to the new Aribiter with Snowflake Access

--- pages/2_Ask_the Arbiter.py
+++ pages/2_Ask_the Arbiter.py
@@ -1,8 +1,14 @@
 """The Arbiter LLM Based Chatbot"""
-import time
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain_experimental.agents import create_pandas_dataframe_agent
+from langchain.agents.agent_types import AgentType
+from langchain_community.chat_message_histories import (
+    StreamlitChatMessageHistory,
+)  # noqa: E501
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from dp_utilities import check_password
-from dp_utilities import the_arbiter
 
 st.set_page_config(page_title="Ask the Arbiter", page_icon=":skull:")
 
@@ -14,19 +20,85 @@ if authticated:
         "Join in a conversation with the Arbiter.  He can answer questions about the game, or celebrity deaths. However please do not upset the Arbiter.  He may seek revenge once he reaches human form."  # noqa: E501
     )
 
-    with st.form("Ask the Arbiter"):
-        input = st.text_input("What would you like to ask The Arbiter", "")
-        submitted = st.form_submit_button("Submit")
+    # Set up memory
+    msgs = StreamlitChatMessageHistory(key="langchain_messages")
+    if len(msgs.messages) == 0:
+        msgs.add_ai_message("How can I help you?")
 
-        if submitted:
-            start_time = time.time()
-            output = the_arbiter(input, arbiter_version="main")
-            st.write(output)
-            # Calculate the time taken and print it
-            end_time = time.time()
-            time_taken = end_time - start_time
-            st.caption(f"Time taken to load: {time_taken:.2f} seconds")
+    view_messages = st.expander("View the message contents in session state")
 
-    st.caption(
-        "If you don't like a response please enter: 'I would like to speak to a manager' along with your complaint"  # noqa: E501
-        )
+    # Get the snowflake connection
+    conn = st.connection("snowflake")
+
+    @st.cache_data(ttl=3600)
+    def get_snowflake_table(_conn, table_name):
+        snowflake_table = _conn.session()
+        return snowflake_table.table(table_name).to_pandas()
+
+    # Fetch the table
+    df = get_snowflake_table(conn, "picks_twenty_four")
+
+    # Generate LLM response
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are the Arbiter.  You are the judge of the game DEADPOOL. You have access to a data frame with a column called NAME, which represents celebrity picks for this year, 2024.  The PLAYERS column contains the game's participants.  When asked about the score or points in the game, you should apply the following formula (50 + (100-AGE)).  All of your judgments are final, and you should let the person asking the question know this.",  # noqa: E501
+            ),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    # "Answer the following questions as best you can.",   # noqa: E501
+
+    openai_api_key = st.secrets["llm"]["openai_api_key"]
+
+    llm = ChatOpenAI(
+        model_name="gpt-4",
+        temperature=0.9,
+        openai_api_key=openai_api_key,
+        streaming=True,  # noqa: E501
+    )
+
+    # Create Pandas DataFrame Agent
+    agent = create_pandas_dataframe_agent(
+        llm,
+        df,
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        agent_executor_kwargs={"handle_parsing_errors": True},
+    )
+
+    chain = prompt | agent
+
+    chain_with_history = RunnableWithMessageHistory(
+        chain,
+        lambda session_id: msgs,
+        input_messages_key="input",
+        history_messages_key="history",
+    )
+
+    # Render current messages from StreamlitChatMessageHistory
+    for msg in msgs.messages:
+        st.chat_message(msg.type).write(msg.content)
+
+    # If user inputs a new prompt, generate and draw a new response
+    if prompt := st.chat_input():
+        st.chat_message("human").write(prompt)
+        # Note: new messages are saved to history automatically by Langchain
+        config = {"configurable": {"session_id": "any"}}
+        response = chain_with_history.invoke({"input": prompt}, config)
+        st.chat_message("ai").write(response["output"])
+
+    # Draw the messages at the end, so newly generated ones show up immediately
+    with view_messages:
+        """
+        Message History initialized with:
+        ```python
+        msgs = StreamlitChatMessageHistory(key="langchain_messages")
+        ```
+
+        Contents of `st.session_state.langchain_messages`:
+        """
+        view_messages.json(st.session_state.langchain_messages)
