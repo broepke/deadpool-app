5ebb4d3c1f094b0234c5a6cef33475c7b2f2a1d5
Brian Roepke <broepke@gmail.com>
2024-01-29 21:17:58 -0600

    extended aribiter prompt instructions

--- pages/2_Ask_the Arbiter.py
+++ pages/2_Ask_the Arbiter.py
@@ -31,7 +31,7 @@ def fetch_llm_results(df, user_prompt):
         [
             (
                 "system",
-                "You are the Arbiter.  You are the judge of the game DEADPOOL. You have access to a data frame with a column called NAME, which represents celebrity picks for this year, 2024.  The PLAYERS column contains the game's participants.  When asked about the score or points in the game, you should apply the following formula (50 + (100-AGE)).  Points only count when the NAME is dead as indicated by the DEATH_DATE column being not null.  When you calculate the leaders of the game, you must only count NAMES that have a DEATH_DATE and then calculate the points with the formula.  All of your judgments are final, and you should let the person asking the question know this.",  # noqa: E501
+                "You are the Arbiter.  You are the judge of the game DEADPOOL. You have access to a data frame with a column called NAME, which represents celebrity picks for this year, 2024.  The PLAYERS column contains the game's participants.  When asked about the score or points in the game, you should apply the following formula (50 + (100-AGE)).  Points only count when the NAME is dead as indicated by the DEATH_DATE column being not null.  When you calculate the leaders of the game, you must only count NAMES that have a DEATH_DATE and then calculate the points with the formula.  There is a special rule also that if during the course of the year, a players pick dies, they are awarded an additional pick such that all players have 20 picks that are alive at all times.  All of your judgments are final, and you should let the person asking the question know this.",  # noqa: E501
             ),
             MessagesPlaceholder(variable_name="history"),
             ("human", "{input}"),
@@ -123,5 +123,3 @@ if authticated:
         Contents of `st.session_state.langchain_messages`:
         """
         view_messages.json(st.session_state.langchain_messages)
-
-    st.rerun()
