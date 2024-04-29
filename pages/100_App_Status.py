ae4570b7e4bc06dae5cb83d2527ea55dd534adfe
Brian Roepke <broepke@gmail.com>
2024-04-29 13:57:36 -0500

    Added simple service update page

---
+++ pages/100_App_Status.py
@@ -0,0 +1,26 @@
+"""
+List of all scoring rules
+"""
+import streamlit as st
+import requests
+from dp_utilities import check_password
+
+st.set_page_config(page_title="App Status", page_icon=":skull:")
+
+st.title("App Status :skull_and_crossbones:")
+
+email, user_name, authenticated = check_password()
+if authenticated:
+
+    def check_url(url):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                st.success("The GitHub Updater service is running")
+            else:
+                st.warning("WARNING: the sevice returns a {response.status_code} status code")
+        except requests.exceptions.RequestException as e:
+            st.error(f"Error checking {url}: {e}")
+
+    check_url("https://deadpool.dataknowsall.com:5000/health_check")
+    
